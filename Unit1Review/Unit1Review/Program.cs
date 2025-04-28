4930fc7c852e6291b6f9a6e73bf52cf9ccbd153c
ReggieMW <reggiewoods@msn.com>
2025-04-28 15:21:25 -0500

    Add files via upload

--- Unit1Review/Unit1Review/Program.cs
+++ Unit1Review/Unit1Review/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please choose a shape to calculate the area:\n1. Circle\n2. Triangle\n3. Rectabgle\n4. Square");
+        Console.WriteLine(
+            "Please choose a shape to calculate the area:\n1. Circle\n2. Triangle\n3. Rectabgle\n4. Square");
         var selection = Console.ReadLine();
 
         switch (selection)
@@ -25,24 +26,24 @@ class Program
                 Console.WriteLine("Invalid entry. Please enter a number.");
                 break;
         }
-        
+
         static void CircleArea()
         {
             Console.WriteLine("Enter the radius of the circle:");
             double radius = Convert.ToDouble(Console.ReadLine());
-            
+
             double areaC = Math.PI * radius * radius;
-           Console.WriteLine($"Area = {areaC}");
+            Console.WriteLine($"Area = {areaC}");
         }
 
         static void TriangleArea()
         {
             Console.WriteLine("Enter the base length of the triangle:");
             double baseLength = Convert.ToDouble(Console.ReadLine());
-            
+
             Console.WriteLine("Enter the height of the triangle:");
             double height = Convert.ToDouble(Console.ReadLine());
-            
+
             double areaT = 0.5 * baseLength * height;
             Console.WriteLine($"Area = {areaT}");
         }
@@ -51,7 +52,7 @@ class Program
         {
             Console.WriteLine("Enter the length of the ractabgle:");
             double length = Convert.ToDouble(Console.ReadLine());
-            
+
             Console.WriteLine("Enter the width of the rectangle:");
             double width = Convert.ToDouble(Console.ReadLine());
 
@@ -62,13 +63,10 @@ class Program
         static void SquareArea()
         {
             Console.WriteLine("Enter the side length of the square:");
-            double side = Convert.ToDouble(Console.ReadLine());    
+            double side = Convert.ToDouble(Console.ReadLine());
 
             double areaS = side * side;
             Console.WriteLine($"Area = {areaS}");
         }
-    
-
-
-
+    }
 }
